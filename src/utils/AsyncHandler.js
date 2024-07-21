73c822482983a71478192fdc8d21d71ac2de0b05
chocoboy2004 <iamrahulghoshinfo@gmail.com>
2024-07-22 02:00:10 +0530

    creating utility files

---
+++ src/utils/AsyncHandler.js
@@ -0,0 +1,9 @@
+const AsyncHandler = (handleRequest) => {
+    return (req, res, next) => {
+        Promise
+        .resolve(handleRequest(req, res, next))
+        .catch(err => next(err))
+    }
+}
+
+export default AsyncHandler
